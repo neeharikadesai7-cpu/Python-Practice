4d4dc7d7d846b017aafc2292c9d9c7b00fdf4d1c
neeharikadesai7-cpu <neeharikadesai7@gmail.com>
2026-07-10 22:58:01 +0530

    Intialized

---
+++ first-chapter/addition.py
@@ -0,0 +1,15 @@
+a=5
+b=7
+print("a+b=",a+b)
+
+
+
+a=int(input("Enter number a:"))
+b=int(input("Enter number b:"))
+print("a+b =", a+b)
+
+# multi line comment 
+# given by three double quotes
+# and also by three single quotes
+# and by using hash symbol at the beginning of each line
+# by using control + / in pycharm
---
+++ first-chapter/variables.py
@@ -0,0 +1,18 @@
+age=19
+name="Neeharika"
+Qualification= "Bachelor's in Computer Science"
+
+print("My name is", name)
+print("I am", age, "years old")
+print("My qualification is", Qualification)
+
+print(type(name))
+print(type(age))
+print(type(Qualification))
+
+age=20
+age= False
+print(type(age))
+
+a= None
+print(type(a))
