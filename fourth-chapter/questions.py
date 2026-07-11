c0376f794b0582123c47b85d3be24aaf2fd3b93a
neeharikadesai7-cpu <neeharikadesai7@gmail.com>
2026-07-11 21:39:11 +0530

    New

---
+++ fourth-chapter/questions.py
@@ -0,0 +1,25 @@
+# python = {
+#     "table":("a piece of furniture","lists of facts and figures"),
+#     "cat":"a small animal"
+# }
+# print(python)
+
+# set={ "C++", "Python", "Java" ,"JavaScript", "C#", "Ruby", "Go", "Swift","Java"}
+
+# print(set)
+# print(len(set))
+
+# dict = {}
+
+# mark1 = input("Enter the marks of the student: ")
+# mark2 = input("Enter the marks of the student: ")        
+# mark3 = input("Enter the marks of the student: ")
+
+# dict.update({"Mark1": mark1, "Mark2": mark2, "Mark3": mark3})
+# print(dict)
+
+s={9,'''9.0'''}
+print(s)
+
+
+
