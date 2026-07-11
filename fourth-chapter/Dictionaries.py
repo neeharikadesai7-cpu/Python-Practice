5a6334cd2003fff7ad187f1ad71343ca068cacf3
neeharikadesai7-cpu <neeharikadesai7@gmail.com>
2026-07-11 20:35:35 +0530

    Dict

--- fourth-chapter/Dictionaries.py
+++ fourth-chapter/Dictionaries.py
@@ -6,3 +6,25 @@ info = {
 }
 print(info)
 
+print(info["age"])
+print(type(info))
+
+print(type(info["age"]))
+
+info.update({
+    "Address": "Maharashtara"
+})
+print(info)
+
+if "name" in info:
+    print("Name is present in the dictionary")
+else:
+    print("Name is not present in the dictionary")
+
+print(info.keys())
+print(info.values())
+print (info.items())
+
+print(info.get("name"))
+
+print(list(info.keys()))
