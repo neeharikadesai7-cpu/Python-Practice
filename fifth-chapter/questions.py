68c1cac7ad79f356211b9d75dc968c6621836646
neeharikadesai7-cpu <neeharikadesai7@gmail.com>
2026-07-12 20:26:27 +0530

    new

---
+++ fifth-chapter/questions.py
@@ -0,0 +1,55 @@
+#q1
+
+# i =1 
+
+# while i<= 100:
+#     print(i)
+#     i += 1
+
+# #q2
+
+# i = 100
+
+# while i>=1: #stop condition
+#     print(i)
+#     i -=1
+
+#q3
+
+# n=int(input("Enter a number:"))
+
+# i=1
+# while i<= 10:
+#     print(n, "*",i, "=",n*i)
+#     i += 1
+
+#q4
+
+# i = 1
+
+# while i<=10:
+#     print(i*i)
+#     i += 1
+
+#q5 
+
+# num = [1, 4,9, 16, 25, 36, 49, 64, 81, 100]
+
+# i = 0
+# while i < len(num):
+#     print(num[i]) #nums[0], nums[1], nums[2]...
+#     i += 1
+
+#q6
+
+num = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+x= 36
+i = 0
+while i < len(num):
+    if num[i] ==x:
+        print("Found the number!" ,num[i])
+    else:
+        print("not found")
+    i += 1
+
+
