2a57d0652e746481e60849ecd02f448f3185a3fb
neeharikadesai7-cpu <neeharikadesai7@gmail.com>
2026-07-12 21:30:44 +0530

    Questions

---
+++ fifth-chapter/practice.py
@@ -0,0 +1,12 @@
+# for i in range(1,101,1):
+#     print(i)
+
+# for i in range(100,0,-1):
+#     print(i)
+
+# x= int(input("Enter a number:"))
+
+# for i in range(1, 11,):
+#     print(x*i)
+
+    
