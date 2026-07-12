ea41bd704fc03b9be1020a860a0c3bf344c11ad9
neeharikadesai7-cpu <neeharikadesai7@gmail.com>
2026-07-12 19:45:40 +0530

    Loops

---
+++ fifth-chapter/while.py
@@ -0,0 +1,17 @@
+# count = 1
+# while count <= 5:
+#     print("Hello World")
+#     count += 1
+
+
+# i = 1
+# while i <= 50:
+#     print("Neeharika", i)
+#     i += 1
+
+
+
+a = 5
+while a> 0:
+    print(a)
+    a -= 1
