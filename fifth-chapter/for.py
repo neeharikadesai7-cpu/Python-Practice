5067a751224d90f26eb92f679887e3896aa242e4
neeharikadesai7-cpu <neeharikadesai7@gmail.com>
2026-07-12 21:01:42 +0530

    update

---
+++ fifth-chapter/for.py
@@ -0,0 +1,12 @@
+num = [1, 2, 3, 4, 5]
+
+for n in num:
+    print(n)
+
+
+tup= (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+for t in tup:
+    print(t)
+
+
--- fifth-chapter/questions.py
+++ fifth-chapter/questions.py
@@ -42,12 +42,29 @@
 
 #q6
 
-num = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
-x= 36
-i = 0
-while i < len(num):
-    if num[i] ==x:
-        print("Found the number!" ,i)
-    i += 1
+# num = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+# x= 36
+# i = 0
+# while i < len(num):
+#     if num[i] ==x:
+#         print("Found the number!" ,i)
+#     i += 1
 
 
+#q7
+
+# num = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# for n in num:
+#     print(n)
+
+num = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+x = int(input("Enter a number to search:"))
+for n in num:
+    if n == x:
+        print("Found the number", n , "at index", num.index(n))
+        break
+    else:
+        print("Finding...")
+        
