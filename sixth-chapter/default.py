8fb896405cf9cac794081ce57d360d578a9b44be
neeharikadesai7-cpu <neeharikadesai7@gmail.com>
2026-07-14 20:30:26 +0530

    New

---
+++ sixth-chapter/default.py
@@ -0,0 +1,6 @@
+def cal_product(a=1,b=1):
+    print(a*b)
+    return(a*b)
+
+cal_product(5,10)
+
--- sixth-chapter/function.py
+++ sixth-chapter/function.py
@@ -4,3 +4,17 @@ def calc_sum(a,b):
     return sum
 
 calc_sum(2,3)
+
+def print_hello():
+    print("Hello!")
+
+output=print_hello()
+print(output)
+
+def calc_avg(a,b,c):
+    sum = a +b +c 
+    avg = sum/3
+    print(avg)
+    return avg
+
+calc_avg(1,2,3)
