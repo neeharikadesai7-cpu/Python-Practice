7db88dd5930ab9aa7a0a0ac8de9b6bf2c2765d5a
neeharikadesai7-cpu <neeharikadesai7@gmail.com>
2026-07-15 22:44:09 +0530

    New

--- seventh-chapter/seven.py
+++ seventh-chapter/seven.py
@@ -1,5 +1,9 @@
-f = open("dem.txt" , "r")
-data = f.read()
-print(data)
-print(type(data))
-f.close()
+with  open("dem.txt" , "r") as f:
+    line1 = f.readline()
+    print(line1)
+
+    line2= f.readline()
+    print(line2)
+    
+
+    print(type(line1))
