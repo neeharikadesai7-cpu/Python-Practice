41f88a68fa88646cc41eec5a9ab9cffbab37bc6c
neeharikadesai7-cpu <neeharikadesai7@gmail.com>
2026-07-17 23:43:07 +0530

    OOP2

---
+++ ninth-chapter/del.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self,name):
+        self.name = name 
+
+s1 = "Neeharika"
+print(s1)
+del s1
+print(s1)
---
+++ ninth-chapter/private.py
@@ -0,0 +1,7 @@
+class Person:
+    __name = "anonymous"
+
+
+
+p1 = Person()
+print(p1.__name)
